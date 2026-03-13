2c1a7502fe359db06f181de750bad6fc43f78037
Zach Kelling <z@zeekay.io>
2026-03-12 23:17:29 -0700

    feat: @hanzo/zap is now a thin re-export wrapper over @zap-proto/zap
    Replace 1000+ lines of copied code with re-exports from @zap-proto/zap.
    Add sub-path exports: ./protocol, ./transport, ./rpc, ./error, ./types.

---
+++ hanzo-dev/zap/sdk/typescript/src/rpc.ts
@@ -0,0 +1,4 @@
+/**
+ * Re-export ZAP RPC layer from canonical @zap-proto/zap.
+ */
+export * from '@zap-proto/zap/rpc';
---
+++ hanzo-dev/zap/sdk/typescript/src/transport.ts
@@ -0,0 +1,4 @@
+/**
+ * Re-export ZAP transports from canonical @zap-proto/zap.
+ */
+export * from '@zap-proto/zap/transport';
